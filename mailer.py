34d6490af5d6c47c5252c8f0868d53fd3a507ffd
Amirlabai <amirlabay@gmail.com>
2025-10-18 16:42:45 +0300

    Refactor email notifications to use HTML formatting and improve messaging clarity

--- mailer.py
+++ mailer.py
@@ -13,12 +13,20 @@ def send_new_user_notification(app, user_email):
             current_app.logger.warning("No admin users found to send new user notification.")
             return
 
+        # Generate the URL for the admin pending page
+        pending_url = url_for('admin.admin_pending', _external=True)
+
         msg = Message(
             'New User Registration',
             sender=config.MAIL_DEFAULT_SENDER,
             recipients=admin_emails
         )
-        msg.body = f"A new user with the email {user_email} has registered and is waiting for approval."
+        msg.html = f"""
+        <p>Hello Admin,</p>
+        <p>A new user with the email <b>{user_email}</b> has registered and is awaiting approval.</p>
+        <p>Please review the request on the <a href="{pending_url}">Pending Approvals</a> page.</p>
+        <p>Thank you!</p>
+        """
         try:
             mail.send(msg)
             current_app.logger.info(f"Admin notification sent for {user_email} to: {', '.join(admin_emails)}")
@@ -28,12 +36,21 @@ def send_new_user_notification(app, user_email):
 def send_approval_email(app, user_email):
     """Sends an email to the user when their account is approved."""
     with app.app_context():
+        # Generate the URL for the login page
+        login_url = url_for('auth.login', _external=True)
+
         msg = Message(
             'Your Account has been Approved!',
             sender=config.MAIL_DEFAULT_SENDER,
             recipients=[user_email]
         )
-        msg.body = "Congratulations! Your account has been approved by an administrator. You can now log in."
+        msg.html = f"""
+        <p>Hello,</p>
+        <p>Congratulations! Your account for Merkaz_lib has been approved by an administrator.</p>
+        <p>You can now log in to access the file library.</p>
+        <p><a href="{login_url}">Click here to Login</a></p>
+        <p>Thank you!</p>
+        """
         try:
             mail.send(msg)
             current_app.logger.info(f"Approval email sent to {user_email}")
@@ -48,7 +65,12 @@ def send_denial_email(app, user_email):
             sender=config.MAIL_DEFAULT_SENDER,
             recipients=[user_email]
         )
-        msg.body = "We regret to inform you that your registration has been denied at this time."
+        msg.html = """
+        <p>Hello,</p>
+        <p>We regret to inform you that your registration for Merkaz_lib has been denied at this time.</p>
+        <p>If you believe this was a mistake, please contact an administrator.</p>
+        <p>Thank you.</p>
+        """
         try:
             mail.send(msg)
             current_app.logger.info(f"Denial email sent to {user_email}")
@@ -64,7 +86,12 @@ def send_password_reset_email(app, user_email, token):
             sender=config.MAIL_DEFAULT_SENDER,
             recipients=[user_email]
         )
-        msg.body = f"Click the following link to reset your password: {reset_url}"
+        msg.html = f"""
+        <p>Hello,</p>
+        <p>You requested a password reset. Click the link below to set a new password. This link will expire in one hour.</p>
+        <p><a href="{reset_url}">Reset Your Password</a></p>
+        <p>If you did not request this, please ignore this email.</p>
+        """
         try:
             mail.send(msg)
             current_app.logger.info(f"Password reset email sent to {user_email}")
